51260ec1cbb801b0e599869cbcd7361d14b8260f
Alan Adi P <96643671+WallHackBotz@users.noreply.github.com>
2022-03-11 09:18:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['62895401928948', '62859328255152'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -30,10 +30,10 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'arif️fb'
-global.author = 'amel'
+global.packname = 'AlanGans'
+global.author = 'Bot'
 
-global.wm = '© ariffb & melcanz'
+global.wm = '© AlanGans'
 global.wait = '_*tunggu sedang diproses...*_'
 global.eror = '_*Server Error*_'
 global.benar = '✅'
